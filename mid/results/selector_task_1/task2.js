ca343fe98002c1d7241cfb76af9cdab1d87faf9e
azzer <anner@lover.com>
2021-04-29 07:55:46 +0200

    Adding new task for this future hacker

--- mid/results/selector_task_1/task2.js
+++ mid/results/selector_task_1/task2.js
@@ -1,16 +1,23 @@
 /*- Link the HTML file to the JS SCRIPT
-	- Select all the LI items and PRINT ONE BY ONE IN THE CONSOLE
-	* This is an extra, read about HOW TO GET THE CLASS OF AN HTML ELEMENT and print next to the element too*/
+    - Select all the LI items and PRINT ONE BY ONE IN THE CONSOLE
+    * This is an extra, read about HOW TO GET THE CLASS OF AN HTML ELEMENT and print next to the element too*/
 
-    const li = document.querySelectorAll("li")
-    console.log(li)
-    const ul = document.querySelectorAll("ul")
-    console.log(ul)
+const li = document.querySelectorAll("li")
+console.log(li)
+const ul = document.querySelectorAll("ul")
+console.log(ul)
 
-    
 
-    for (let i = 0; i < li.length; i++){
-        console.log (li[i].classList.value)
+
+for (let i = 0; i < li.length; i++) {
+    console.log(li[i].classList.value)
+}
+
+// MY SOLUTION, is more legible u see
+const ul = document.querySelectorAll("ul li")
+for (let i = 0; i < ul.length; i++) {
+    const li = ul[i]
+    console.log(li.classList.value)
     
-    }
-    
+}
+
